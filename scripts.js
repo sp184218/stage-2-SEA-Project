bdcddc63401988d374cb0d8c58b35e5e20cac62c
sp184218 <sp184218@gmail.com>
2025-04-12 20:46:50 -0700

    I updated the index.html with adding my own catalog dvd version which included connecting it to my styles.css and javascript. My JS was able to categorize my BIG 3 M'S. This is only the sample to see if the functionality worked properly and now main menu is complete, but need to make subpages for my big 3 M's.

--- scripts.js
+++ scripts.js
@@ -1,97 +1,67 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
-
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
+// Sample data array for the Big 3 M’s
+const categories = [
+  {
+    title: "Movies I Love 🎬",
+    image: "images/movies.jpg",
+    points: ["Inception", "The Matrix", "Spirited Away"]
+  },
+  {
+    title: "Music I Love 🎵",
+    image: "images/music.jpg",
+    points: ["Lauryn Hill", "Frank Ocean", "Chillhop & Lofi"]
+  },
+  {
+    title: "Math Concepts I Love ➕➗",
+    image: "images/math.jpg",
+    points: ["Fractals", "Golden Ratio", "Symmetry"]
   }
+];
+// Function to create cards for each category
+function createCards() {
+  const container = document.getElementById("card-container");
+
+  categories.forEach((category) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const content = document.createElement("div");
+    content.classList.add("card-content");
+
+    const title = document.createElement("h2");
+    title.textContent = category.title;
+
+    const img = document.createElement("img");
+    img.src = category.image;
+    img.alt = category.title;
+
+    const ul = document.createElement("ul");
+    category.points.forEach((point) => {
+      const li = document.createElement("li");
+      li.textContent = point;
+      ul.appendChild(li);
+    });
+
+    content.appendChild(title);
+    content.appendChild(img);
+    content.appendChild(ul);
+    card.appendChild(content);
+    container.appendChild(card);
+  });
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
-
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
-
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
-
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
-}
-
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
+// Example feature functions
 function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
-  );
+  alert("Sydematic’s Big 3 M’s are iconic. Be inspired!");
 }
 
 function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
+  const container = document.getElementById("card-container");
+  if (container.lastChild) {
+    container.removeChild(container.lastChild);
+  }
 }
+
+// Initialize on page load
+window.onload = () => {
+  createCards();
+};
